6754d14d1487eedcdeadb6e5ff48930142e1b130
Hritwick De <hritwick14@gmail.com>
2025-01-18 00:20:23 +0530

    real test done

--- src/services/GlobalRegistry.ts
+++ src/services/GlobalRegistry.ts
@@ -1,34 +1,47 @@
-import {
-  Bank,
-  BankAccount,
-  BankAccountRegistry,
-  BankRegistry,
-  Transaction,
-  TransactionServiceRegistry,
-  User,
-  UserRegistry,
-} from '@/types/Common';
+import { BankId, BankAccountId, UserId } from "@/types/Common";
+import type Bank from "@/models/bank";
+import type BankAccount from "@/models/bank-account";
+import type User from "@/models/user";
+import type TransactionService from "./TransactionService";
 
 class GlobalRegistry {
-  private static bankRegistry: BankRegistry = [];
-  private static bankAccountRegistry: BankAccountRegistry = [];
-  private static userRegistry: UserRegistry = [];
-  private static transactionServiceRegistry: TransactionServiceRegistry = [];
+  private static bankRegistry: Bank[] = [];
+  private static bankAccountRegistry: BankAccount[] = [];
+  private static userRegistry: User[] = [];
+  private static transactionServiceRegistry: TransactionService[] = [];
 
   static addBank(bank: Bank) {
     this.bankRegistry.push(bank);
   }
-
   static addBankAccount(bankAccount: BankAccount) {
     this.bankAccountRegistry.push(bankAccount);
   }
-
   static addUser(user: User) {
     this.userRegistry.push(user);
   }
-
-  static addTransaction(transaction: Transaction) {
+  static addTransaction(transaction: TransactionService) {
     this.transactionServiceRegistry.push(transaction);
+  }
+
+  static getBank(bankId: BankId) {
+    return this.bankRegistry.filter((bank: Bank) => bank.getId() === bankId)[0];
+  }
+  static getBankAccount(bankAccountId: BankAccountId) {
+    return this.bankAccountRegistry.filter((bankAccount: BankAccount) => bankAccount.getId() === bankAccountId)[0];
+  }
+  static getUser(userId: UserId) {
+    return this.userRegistry.filter((user: User) => user.getId() === userId)[0];
+  }
+
+  static getBankAccountIdsByUserIdAndBankId(userId: UserId, bankId: BankId) {
+    const user = this.userRegistry.filter((user: User) => user.getId() === userId)[0];
+    const userAccountIdList = user.getAccountIds();
+    const finalList = userAccountIdList.filter((accountId: BankAccountId) => {
+      const account = this.getBankAccount(accountId);
+      return account.getBankId() === bankId;
+    })
+
+    return finalList
   }
 
   static clear() {
